d517fc38ca69c375c16cf8b25859f7f0f749e2bd
jayunani <j.ayunani@gmail.com>
2017-04-11 11:26:19 -0700

    preventing page jump3

--- app/components/EventDetails/MendhiDetails.jsx
+++ app/components/EventDetails/MendhiDetails.jsx
@@ -38,7 +38,7 @@ class MendhiDetails extends React.Component {
           <div className='rsvp-form'>
             <img src='./rsvp.png' />
             <div className='event-form'>
-              <iframe id='collapsed-form-mendhi' className='collapse collapsed-div'  src="https://docs.google.com/forms/d/e/1FAIpQLScjffUYeNEWlK9JijhzoS7i3orOCBf7P3cQgcgAukZoQlgSZg/viewform?embedded=true about:blank" width="450" height="400" frameborder="0" marginheight="0" marginwidth="0" >Loading...</iframe>
+              <iframe id='collapsed-form-mendhi' className='collapse collapsed-div'  src="https://docs.google.com/forms/d/e/1FAIpQLScjffUYeNEWlK9JijhzoS7i3orOCBf7P3cQgcgAukZoQlgSZg/viewform?embedded=true about:blank" width="450" height="400" >Loading...</iframe>
             </div>
           </div>
           
